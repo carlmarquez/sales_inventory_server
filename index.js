7c484d9c188689d751268f889a3d90d8a06a2a12
hannalalia <63851948+hannalalia@users.noreply.github.com>
2021-05-07 23:08:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors(
     {
         methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-        origin: 'https://jars-cellular.netlify.app',
+        origin: 'https://jars-cellular.netlify.app/',
     }
 ))
 
